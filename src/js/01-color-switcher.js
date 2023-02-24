b85f2dd0d8c1cc0a3c202172f25422123f241bfc
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-02-24 22:38:44 +0200

    update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
 
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+     stopBtn.removeAttribute('disabled');
+     startBtn.setAttribute('disabled', true);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
