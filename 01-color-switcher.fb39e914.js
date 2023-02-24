e84793fa1249d715a044f24024c60704ba04cc21
KaterynaKozlovska <111512281+KaterynaKozlovska@users.noreply.github.com>
2023-02-24 21:38:51 +0000

    Deploying to gh-pages from @ KaterynaKozlovska/goit-js-hw-09@4d04f680e033a6adea095fc958bb95a0576ab86b 🚀

---
+++ 01-color-switcher.fb39e914.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.removeAttribute("disabled"),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(timerId),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.fb39e914.js.map
