bc12c18b36b49ddfae7ce2c81d038ff20d764bbf
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-02-25 22:34:38 +0200

    update 03-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,19 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-const startBtn = document.querySelector("button[data-start]");
-const input = document.querySelector("#datetime-picker");
-
+const startBtn = document.querySelector('button[data-start]');
+const input = document.querySelector('#datetime-picker');
+const seconds = document.querySelector('span[data-seconds]');
+const minutes = document.querySelector('span[data-minutes]');
+const hours = document.querySelector('span[data-hours]');
+const days = document.querySelector('span[data-days]');
 
 startBtn.setAttribute('disabled', true);
 
-const date = new Date(input.value).getTime();
-console.log(date);
+// const date = new Date(input.value).getTime();
+// console.log(date);
+
+// const currentDate = Date.now();
 
 const options = {
   enableTime: true,
@@ -16,6 +21,60 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < new Date()) {
+      window.alert('Please choose a date in the future');
+      startBtn.setAttribute('disabled', true);
+    } else {
+      startBtn.removeAttribute('disabled');
+    }
   },
 };
+
+flatpickr(input, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+startBtn.addEventListener('click', () => {
+  let timer = setInterval(() => {
+    let countdown = new Date(input.value) - new Date();
+    startBtn.disabled = true;
+    if (countdown >= 0) {
+      let timeObject = convertMs(countdown);
+      days.textContent = addLeadingZero(timeObject.days);
+      hours.textContent = addLeadingZero(timeObject.hours);
+      minutes.textContent = addLeadingZero(timeObject.minutes);
+      seconds.textContent = addLeadingZero(timeObject.seconds);
+      if (countdown <= 10000) {
+        // timerHtml.style.color = 'tomato';
+      }
+    } else {
+      // timerHtml.style.color = 'black';
+      clearInterval(timer);
+    }
+  }, 1000);
+});
